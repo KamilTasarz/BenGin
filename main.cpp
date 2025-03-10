3a99e54bc7d80cb3aa6d729374c2e57e86b45de0
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2025-03-10 15:55:46 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -80,25 +80,25 @@ float normals[] = {
 
 };
 
-int indices[]{
-    0, 1, 2, //front
-    0, 2, 3, 
-
-    4, 5, 1, //prawy
-    4, 1, 0,
-
-    4, 0, 3, //gora
-    4, 3, 7,
-
-    3, 2, 6, //lewy
-    3, 6, 7,
-
-    5, 1, 2, //dol
-    5, 2, 6,
-
-    4, 5, 7, //tyl
-    7, 5, 6
-};
+//int indices[]{
+//    0, 1, 2, //front
+//    0, 2, 3, 
+//
+//    4, 5, 1, //prawy
+//    4, 1, 0,
+//
+//    4, 0, 3, //gora
+//    4, 3, 7,
+//
+//    3, 2, 6, //lewy
+//    3, 6, 7,
+//
+//    5, 1, 2, //dol
+//    5, 2, 6,
+//
+//    4, 5, 7, //tyl
+//    7, 5, 6
+//};
     
 const char* vertexPath = "res/basic.vert";
 const char* fragmentPath = "res/basic.frag";
@@ -273,136 +273,3 @@ bool init() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     return true;
 }
-
-//#include <glad/glad.h>
-//#include <GLFW/glfw3.h>
-//#include <iostream>
-//
-//// Vertex Shader (GLSL)
-//const char* vertexShaderSource = R"(
-//    #version 330 core
-//    layout (location = 0) in vec3 aPos;
-//    void main() {
-//        gl_Position = vec4(aPos, 1.0);
-//    }
-//)";
-//
-//// Fragment Shader (GLSL)
-//const char* fragmentShaderSource = R"(
-//    #version 330 core
-//    out vec4 FragColor;
-//    void main() {
-//        FragColor = vec4(0.2, 0.6, 0.9, 1.0);
-//    }
-//)";
-//
-//// Callback do zmiany rozmiaru okna
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-//    glViewport(0, 0, width, height);
-//}
-//
-//int main() {
-//    // Inicjalizacja GLFW
-//    if (!glfwInit()) {
-//        std::cerr << "Nie można zainicjalizować GLFW!" << std::endl;
-//        return -1;
-//    }
-//
-//    // Ustawienie wersji OpenGL (3.3) i trybu core profile
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//    // Tworzenie okna
-//    GLFWwindow* window = glfwCreateWindow(800, 600, "Kwadrat w OpenGL", NULL, NULL);
-//    if (!window) {
-//        std::cerr << "Nie można utworzyć okna!" << std::endl;
-//        glfwTerminate();
-//        return -1;
-//    }
-//    glfwMakeContextCurrent(window);
-//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-//
-//    // Inicjalizacja GLAD
-//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-//        std::cerr << "Nie można zainicjalizować GLAD!" << std::endl;
-//        return -1;
-//    }
-//
-//    // Współrzędne wierzchołków kwadratu
-//    float vertices[] = {
-//        0.5f,  0.5f, 0.0f,  // Prawy górny
-//        0.5f, -0.5f, 0.0f,  // Prawy dolny
-//       -0.5f, -0.5f, 0.0f,  // Lewy dolny
-//       -0.5f,  0.5f, 0.0f   // Lewy górny
-//    };
-//
-//    // Indeksy dla rysowania kwadratu
-//    unsigned int indices[] = {
-//        0, 1, 2,  // Pierwszy trójkąt
-//        0, 2, 3   // Drugi trójkąt
-//    };
-//
-//    // Tworzenie VAO, VBO, EBO
-//    unsigned int VAO, VBO, EBO;
-//    glGenVertexArrays(1, &VAO);
-//    glGenBuffers(1, &VBO);
-//    glGenBuffers(1, &EBO);
-//
-//    glBindVertexArray(VAO);
-//
-//    // Bufor wierzchołków
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//
-//    // Bufor indeksów
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-//
-//    // Atrybut wierzchołków
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(0);
-//
-//    // Odwiązanie buforów
-//    glBindBuffer(GL_ARRAY_BUFFER, 0);
-//    glBindVertexArray(0);
-//
-//    // Kompilacja i linkowanie shaderów
-//    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-//    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-//    glCompileShader(vertexShader);
-//
-//    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-//    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-//    glCompileShader(fragmentShader);
-//
-//    unsigned int shaderProgram = glCreateProgram();
-//    glAttachShader(shaderProgram, vertexShader);
-//    glAttachShader(shaderProgram, fragmentShader);
-//    glLinkProgram(shaderProgram);
-//
-//    glDeleteShader(vertexShader);
-//    glDeleteShader(fragmentShader);
-//
-//    // Pętla renderowania
-//    while (!glfwWindowShouldClose(window)) {
-//        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT);
-//
-//        glUseProgram(shaderProgram);
-//        glBindVertexArray(VAO);
-//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-//
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    // Zwolnienie zasobów
-//    glDeleteVertexArrays(1, &VAO);
-//    glDeleteBuffers(1, &VBO);
-//    glDeleteBuffers(1, &EBO);
-//    glDeleteProgram(shaderProgram);
-//
-//    glfwTerminate();
-//    return 0;
-//}
