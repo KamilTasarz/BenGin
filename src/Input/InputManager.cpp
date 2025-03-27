f265962353c0cd561c814226a4e3c12e7ad7bd8e
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2025-03-27 15:09:21 +0100

    InputManager: added mapping keys to actions and action callbacks

--- src/Input/InputManager.cpp
+++ src/Input/InputManager.cpp
@@ -1,25 +1,39 @@
 #include "InputManager.h"
 
-void InputManager::registerActionCallback(std::string action_name, ActionCallback callback) {
+// --- REGISTER AND REMOVE ACTION CALLBACKS --- //
 
+void InputManager::registerActionCallback(const std::string& action_name, const ActionCallback& callback) {
 
-
-}
-
-void InputManager::removeActionCallback(std::string, std::string callback_reference) {
-
-
-
-}
-
-void InputManager::mapInputToAction(InputKey key, std::string action) {
-
-
+	// If you query a map and it doesn't exist, it's initialized by default
+	// We emplace_back cause we are making a stack (we want callbacks at the back)
+	// For example: space (on ground) -> jump, space (mid-air) -> double jump, space (back on ground) -> jump
+	// So it's push -> pop
+	_action_callback_mapping[action_name].emplace_back(callback);
 
 }
 
-void InputManager::unmapInputFromAction(InputKey key, std::string action) {
+void InputManager::removeActionCallback(const std::string& action_name, const std::string& callback_reference) {
 
+	// If there exists an action in the map, which reference is the same as the passed
+	// string, the ActionCallback is removed from the map
+	// "Removes any instance of this callback reference from a vector of callbacks"
+	std::erase_if(_action_callback_mapping[action_name], [callback_reference](const ActionCallback& callback) {
+		return callback.callback_reference == callback_reference;
+		});
 
+}
+
+// --- MAP AND UNMAP INPUT TO/FROM ACTIONS --- //
+
+void InputManager::mapInputToAction(InputKey key, const std::string& action) {
+
+	_input_action_mapping[key].emplace(action);
+
+}
+
+void InputManager::unmapInputFromAction(InputKey key, const std::string& action) {
+
+	// Because it's a set it will only be present once (no copies of the same action for one key)
+	_input_action_mapping[key].erase(action);
 
 }
--- src/Input/InputManager.h
+++ src/Input/InputManager.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <unordered_map>
+#include <unordered_set>
 #include <string>
 #include <vector>
 
+#include <algorithm>
 #include <functional>;
 
 #include "InputKey.h"
@@ -23,22 +25,23 @@ public:
 	};
 
 	// We connect a specific action to its callback (which is a method along with a string to reference)
-	void registerActionCallback(std::string action_name, ActionCallback callback);
+	void registerActionCallback(const std::string& action_name, const ActionCallback& callback);
 	// If we want to remove the connection of an action with its callback from the map
-	void removeActionCallback(std::string, std::string callback_reference);
+	void removeActionCallback(const std::string& action_name, const std::string& callback_reference);
 
 	// We associate performing an input (like pressing a certain key) with an action
-	void mapInputToAction(InputKey key, std::string action);
+	void mapInputToAction(InputKey key, const std::string& action);
 	// We use it to make an input no longer be connected to an action
-	void unmapInputFromAction(InputKey key, std::string action);
+	void unmapInputFromAction(InputKey key, const std::string& action);
 
 private:
 
 	// By default both are initialized to empty ({} does that)
 
-	// This maps the enum class "InputKey" to a specified action defined by a string
+	// This maps the enum class "InputKey" to a specified actions defined by a set of strings
 	// Which key press triggers which action
-	std::unordered_map<InputKey, std::string> _input_action_mapping {};
+	// Set allows us to map a single key to multiple actions (different in GUI, different in game)
+	std::unordered_map<InputKey, std::unordered_set<std::string>> _input_action_mapping {};
 
 	// This maps actions (defined by a string) to a vector of "ActionCallback". Holds action callbacks
 	// Holds what should happen when an action is triggered
