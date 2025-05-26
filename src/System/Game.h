18c5c039a9e1ea4023650c33534f3b2f13519e71
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2025-05-26 11:22:56 +0200

    Pewne zmiany Kamila Tasarza

--- src/System/Game.h
+++ src/System/Game.h
@@ -12,7 +12,9 @@ class BoundingBox;
 
 class Game
 {
+
 private:
+
 	bool is_initialized = false;
 
 	int viewX;
@@ -25,6 +27,9 @@ private:
 	std::vector<BoundingBox*> colliders;
 	std::vector<std::shared_ptr<Prefab>>& prefabs;
 
+	// SSAO vectors
+	std::vector<glm::vec3> ssao_kernel;
+
 	SceneGraph* sceneGraph;
 	PostProcessData postProcessData;
 	unsigned int framebuffer, colorTexture, normalTexture, depthRenderbuffer;
@@ -32,13 +37,16 @@ private:
 	void input();
 	void draw();
 	void update(float deltaTime);
+
 public:
+
 	bool play = true;
 	Game(std::vector<std::shared_ptr<Prefab>>& prefabsref);
 
 	void init();
 	void run();
 	void shutdown();
+
 };
 
 #endif // !GAME_H
