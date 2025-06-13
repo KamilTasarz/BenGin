8d38e7002418160517b29a58377455c07faaa065
KamilTasarz <247811@edu.p.lodz.pl>
2025-06-13 16:51:17 +0200

    Merge branch 'new-puzzles' of https://github.com/KamilTasarz/BenGin into new-puzzles

---
+++ src/Gameplay/ShowText.cpp
@@ -0,0 +1,103 @@
+#include "ShowText.h"
+#include "../Basic/Node.h"
+#include "RegisterScript.h"
+#include "../System/GuiManager.h"
+#include "GameManager.h"
+#include "GameMath.h"
+
+REGISTER_SCRIPT(ShowText);
+
+void ShowText::onAttach(Node* owner)
+{
+	this->owner = owner;
+	std::cout << "ShowText::onAttach::" << owner->name << std::endl;
+}
+
+void ShowText::onDetach()
+{
+	owner = nullptr;
+}
+
+void ShowText::onStart()
+{
+	text = "Escape from the gas!";
+	
+	textObject = GuiManager::Instance().findText(9);
+	if (!textObject) {
+		std::cout << "ShowText::onStart: TextObject not found!" << std::endl;
+		return;
+	}
+
+	//textObject->setActive(false);
+	textChars.clear();
+
+	for (char c : text) {
+		textChars.push_back(c);
+	}
+
+	textObject->value = "";
+	isWriting = false;
+}
+
+void ShowText::onUpdate(float deltaTime)
+{
+	if ((!isWriting && !isDeleting) || textChars.empty()) {
+		return;
+	}
+
+	static float timer = 0.0f;
+	timer += deltaTime;
+
+	if (timer >= speed && isWriting) {
+		timer = 0.0f + GameMath::RandomFloat(-0.05, 0.05);
+		if (!textChars.empty()) {
+			textObject->value += textChars.front();
+
+			if (textChars.front() != ' ') {
+				auto* audio = ServiceLocator::getAudioEngine();
+				audio->PlaySFX(audio->writing, GameManager::instance->sfxVolume * 80.f);
+			}
+
+			textChars.erase(textChars.begin());
+		}
+		if (textChars.empty()) {
+			isWriting = false;
+			//textObject->setActive(false);
+		}
+	}
+	else if (timer >= speed && isDeleting) {
+		timer = 0.0f;
+		if (!textObject->value.empty()) {
+			textObject->value.pop_back();
+		}
+		if (textObject->value.empty()) {
+			isDeleting = false;
+			//textObject->setActive(false);
+		}
+	}
+}
+
+void ShowText::onCollisionLogic(Node* other)
+{
+	if (other->getTagName() == "Player") {
+		if (!isWriting) {
+			isWriting = true;
+			//textObject->setActive(true);
+			textObject->value = ""; // Reset text before showing
+			for (char c : text) {
+				textChars.push_back(c);
+			}
+		}
+	}
+}
+
+void ShowText::onExitCollisionLogic(Node* other)
+{
+	if (other->getTagName() == "Player") {
+		isWriting = false;
+		isDeleting = true;
+	}
+}
+
+
+
---
+++ src/Gameplay/ShowText.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Script.h"
+
+class TextObject;
+
+class ShowText : public Script
+{
+public:
+	using SelfType = ShowText;
+
+	VARIABLE(std::string, text);
+	VARIABLE(float, speed);
+
+	//std::string text = "Escape from the gas!";
+
+	TextObject* textObject;
+	bool isWriting = false;
+	bool isDeleting = false;
+	vector<char> textChars;
+
+	ShowText() = default;
+	~ShowText() = default;
+	void onAttach(Node* owner) override;
+	void onDetach() override;
+	void onStart() override;
+	void onUpdate(float deltaTime) override;
+
+	void onCollisionLogic(Node* other) override;
+	void onExitCollisionLogic(Node* other) override;
+
+	std::vector<Variable*> getFields() const override {
+		static Variable textVar = getField_text();
+		static Variable speedVar = getField_speed();
+		return { &textVar, &speedVar };
+	}
+};
+
