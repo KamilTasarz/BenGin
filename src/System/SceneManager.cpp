b10254de24ee6aee76f9fb69ec949a2a4f77ffa0
KamilTasarz <247811@edu.p.lodz.pl>
2025-06-20 19:11:51 +0200

    sceneManager implementation

--- src/System/SceneManager.cpp
+++ src/System/SceneManager.cpp
@@ -1,1 +1,86 @@
-#include "SceneManager.h"
+﻿#include "SceneManager.h"
+#include "../Basic/Node.h"
+#include "../System/Serialization.h"
+#include "../System/PrefabRegistry.h"
+#include <fstream>
+
+SceneManager::~SceneManager()
+{
+	if (sceneGraph != nullptr)
+	{
+		delete sceneGraph;
+		sceneGraph = nullptr;
+	}
+}
+
+void SceneManager::next()
+{
+	if (currentSceneIndex + 1 < scenes.size())
+	{
+		currentSceneIndex++;
+		
+	}
+	else
+	{
+		currentSceneIndex = 0; 
+		
+	}
+	std::vector<std::shared_ptr<Prefab>> rooms = PrefabRegistry::GetRooms();
+	std::vector<std::shared_ptr<Prefab>> puzzles = PrefabRegistry::GetPuzzles();
+	
+	loadScene(scenes[currentSceneIndex].path_name, sceneGraph, rooms, puzzles);
+}
+
+void SceneManager::previous()
+{
+	if (currentSceneIndex - 1 >= 0)
+	{
+		currentSceneIndex--;
+	}
+	else
+	{
+		currentSceneIndex = scenes.size() - 1;
+	}
+	std::vector<std::shared_ptr<Prefab>> rooms = PrefabRegistry::GetRooms();
+	std::vector<std::shared_ptr<Prefab>> puzzles = PrefabRegistry::GetPuzzles();
+
+	loadScene(scenes[currentSceneIndex].path_name, sceneGraph, rooms, puzzles);
+}
+
+void SceneManager::reset()
+{
+	if (currentSceneIndex >= 0 && currentSceneIndex < scenes.size())
+	{
+		std::vector<std::shared_ptr<Prefab>> rooms = PrefabRegistry::GetRooms();
+		std::vector<std::shared_ptr<Prefab>> puzzles = PrefabRegistry::GetPuzzles();
+		loadScene(scenes[currentSceneIndex].path_name, sceneGraph, rooms, puzzles);
+	}
+	else
+	{
+		currentSceneIndex = -1;
+	}
+}
+
+SceneGraph* SceneManager::getCurrentScene()
+{
+
+	return sceneGraph;
+}
+
+void SceneManager::Initialize()
+{
+	std::ifstream file("res/scene/scene_order.json");
+	if (!file) {
+		std::cerr << "Błąd: Nie można otworzyć pliku JSON!\n";
+	}
+	else {
+
+		json sceneData;
+		file >> sceneData;
+		file.close();
+
+		for (json scene : sceneData) {
+			
+		}
+	}
+}
--- src/System/SceneManager.h
+++ src/System/SceneManager.h
@@ -1,16 +1,44 @@
 #pragma once
 #include "../config.h"
 
+class SceneGraph;
+
+struct OrderedScene
+{
+	std::string path_name;
+	int order;
+};
+
 class SceneManager
 {
+private:
+	std::vector<OrderedScene> scenes;
+	int currentSceneIndex = -1;
+	SceneGraph* sceneGraph = nullptr;
+
 public:
 	SceneManager() = default;
-	~SceneManager() = default;
+	~SceneManager();
+
+	SceneManager& Instance()
+	{
+		static SceneManager instance;
+		return instance;
+	}
+
+	//load next scene
+	void next();
+
+	//load previous scene
+	void previous();
+
+	//load current index scene again
+	void reset();
+
+	//get pointer to current scene graph
+	SceneGraph* getCurrentScene();
+
 	void Initialize();
-	void Update(float deltaTime);
-	void Render();
-	void LoadScene(const std::string& sceneName);
-	void UnloadScene(const std::string& sceneName);
 
 };
 
