fa6b641c4abfe973c9e01a33409e68aff8342244
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2025-06-25 21:12:00 +0200

    audio not working

--- src/System/Game.cpp
+++ src/System/Game.cpp
@@ -671,6 +671,10 @@ void Game::shutdown()
 
     glfwSetInputMode(ServiceLocator::getWindow()->window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     unloadSounds();
+
+    auto* audio = ServiceLocator::getAudioEngine();
+    audio->Reset();
+
     glDeleteTextures(1, &colorTexture);
     glDeleteTextures(1, &normalTexture);
     glDeleteTextures(1, &depthTexture);
